94f84731d92f07502599f08a8ae352711b932667
jemelsalby <jemelsalby3@gmail.com>
2022-10-26 18:28:39 +0530

    ra

--- src/main/java/com/nestdigital/Recipe/RecipeApplication.java
+++ src/main/java/com/nestdigital/Recipe/RecipeApplication.java
@@ -1,13 +1,71 @@
 package com.nestdigital.Recipe;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@SpringBootApplication
+@Entity
+@Table(name ="recipe")
 public class RecipeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecipeApplication.class, args);
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String recipeName;
+	private String recipeItem;
+	private String recipeMake;
+	private String recipeQuality;
+
+	public RecipeApplication(int id, String recipeName, String recipeItem, String recipeMake, String recipeQuality) {
+		this.id = id;
+		this.recipeName = recipeName;
+		this.recipeItem = recipeItem;
+		this.recipeMake = recipeMake;
+		this.recipeQuality = recipeQuality;
 	}
 
+	public RecipeApplication() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+
+	public String getRecipeItem() {
+		return recipeItem;
+	}
+
+	public void setRecipeItem(String recipeItem) {
+		this.recipeItem = recipeItem;
+	}
+
+	public String getRecipeMake() {
+		return recipeMake;
+	}
+
+	public void setRecipeMake(String recipeMake) {
+		this.recipeMake = recipeMake;
+	}
+
+	public String getRecipeQuality() {
+		return recipeQuality;
+	}
+
+	public void setRecipeQuality(String recipeQuality) {
+		this.recipeQuality = recipeQuality;
+	}
 }
